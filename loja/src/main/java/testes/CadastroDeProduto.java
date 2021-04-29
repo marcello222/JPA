a7b24685ce5f2748301c7a7fe0a7a762098011ed
HP <HP@192.168.15.7>
2021-04-29 19:58:57 -0300

    Quarto commit

--- loja/src/main/java/testes/CadastroDeProduto.java
+++ loja/src/main/java/testes/CadastroDeProduto.java
@@ -1,40 +1,36 @@
 package testes;
 
-import java.math.BigDecimal;
-
 import javax.persistence.EntityManager;
 
-import dao.CategoriaDAO;
-import dao.ProdutoDAO;
 import modelo.Categoria;
-import modelo.Produto;
 import util.JPAUtil;
 
 public class CadastroDeProduto {
 
 	public static void main(String[] args) {
-		
 		Categoria celulares = new Categoria("CELULARES");
 
-		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
-
-//		// Acessa o banco de dados atraves interface EntityManager que vai fazer a
-//		// ponte em transformar a entidade produto em uma linha na tabela no banco de dados;
-//		EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
-//		EntityManager em = factory.createEntityManager();
-
 		EntityManager em = JPAUtil.getEntityManager();
-		ProdutoDAO produtoDao = new ProdutoDAO(em);
-		CategoriaDAO categoriaDao = new CategoriaDAO(em);
-
-		// EntityManager vai la e antes de persistir pega a transação e inicia.
 		em.getTransaction().begin();
-		
-		categoriaDao.cadastrar(celulares);
-		produtoDao.cadastrar(celulares);
-		
-		em.getTransaction().commit();
-		em.close();
+
+		em.persist(celulares);
+
+		// update após o persist
+		celulares.setNome("XPTO");
+
+		// flush para sincronizar com banco de dados
+		em.flush();
+		// clear para limpar
+		em.clear();
+
+		// retorna a entidade criada do estado DETACHED para o MANAGER
+		celulares = em.merge(celulares);
+		celulares.setNome("1234");
+		em.flush();
+		em.clear();
+		em.remove(celulares);
+		em.flush();
+
 	}
 
 }
