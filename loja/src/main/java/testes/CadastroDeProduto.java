d98b44acbeee4edf888184f8384b542e33c4d83d
HP <HP@192.168.15.7>
2021-04-29 18:23:26 -0300

    Terceiro commit

--- loja/src/main/java/testes/CadastroDeProduto.java
+++ loja/src/main/java/testes/CadastroDeProduto.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import javax.persistence.EntityManager;
 
+import dao.CategoriaDAO;
 import dao.ProdutoDAO;
 import modelo.Categoria;
 import modelo.Produto;
@@ -12,21 +13,26 @@ import util.JPAUtil;
 public class CadastroDeProduto {
 
 	public static void main(String[] args) {
-		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), Categoria.CELULARES);
 		
+		Categoria celulares = new Categoria("CELULARES");
+
+		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
 
 //		// Acessa o banco de dados atraves interface EntityManager que vai fazer a
 //		// ponte em transformar a entidade produto em uma linha na tabela no banco de dados;
 //		EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
 //		EntityManager em = factory.createEntityManager();
-		
-		
+
 		EntityManager em = JPAUtil.getEntityManager();
-		ProdutoDAO dao = new ProdutoDAO(em);
+		ProdutoDAO produtoDao = new ProdutoDAO(em);
+		CategoriaDAO categoriaDao = new CategoriaDAO(em);
+
+		// EntityManager vai la e antes de persistir pega a transação e inicia.
+		em.getTransaction().begin();
 		
-		//EntityManager vai la e antes de persistir pega a transação e inicia. 
-		em.getTransaction().begin();		
-		dao.cadastrar(celular);
+		categoriaDao.cadastrar(celulares);
+		produtoDao.cadastrar(celulares);
+		
 		em.getTransaction().commit();
 		em.close();
 	}
